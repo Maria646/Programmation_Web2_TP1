26ba6df25825d8f420a0610761fd53bd01c28244
Mariama Diaby <diabymariama414@gmail.com>
2023-06-30 18:29:38 -0700

    terminer

--- src/componants/card/Carte.jsx
+++ src/componants/card/Carte.jsx
@@ -36,14 +36,14 @@ const Carte = (props) => {
 
        {/* Jai essayer cette solution ci-dessous en mettant la fonctionnalité du useState dans les componants importer...
        mais cela ne fonctionne pas ! */}
-
-    {/* <ContainerImage  containerImage= {props.containerImage} onMouseEnter ={onEnter} onMouseLeave={onLeave}>
-     <Image src={props.src} alt={props.alt}></Image>
-    </ContainerImage>
+      
+    {/* <ContainerImage  containerImage= {props.containerImage} mouseEntrer ={onEnter} mouseLeave={onLeave}>
+      <Image src={props.src} alt={props.alt}></Image>
+     </ContainerImage>
     <ContainerText containerText= {props.containerText} style={containerTextStyle}>
       <Text type={props.typeTitle}>{props.childrenTitle}</Text>
       <Text type={props.typePara}>{props.childrenPara}</Text>
-    </ContainerText> */}
+    </ContainerText>  */}
     </div>
   )
 }
